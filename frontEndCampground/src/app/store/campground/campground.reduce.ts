42e07608dcc4b01d2e7f56c12a822399d8751d71
milutin2002 <milutinjovanovic2002@gmail.com>
2024-07-08 19:46:39 +0200

    Dodao sam entity i adapter

--- frontEndCampground/src/app/store/campground/campground.reduce.ts
+++ frontEndCampground/src/app/store/campground/campground.reduce.ts
@@ -2,23 +2,24 @@ import {createReducer,on} from '@ngrx/store';
 import { campground } from '../../../models/campground';
 import * as Actions from './campground.action';
 import { Action } from 'rxjs/internal/scheduler/Action';
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
 
-export interface CampgroundState{
+export interface CampgroundState extends EntityState<campground>{
+    selectCampground:number
+}
+export interface CampgroundStateOld{
     campgrounds:campground[],
     selectCampground:number | null
 };
-export const initialState:CampgroundState={
-    campgrounds:[],
-    selectCampground:null
-};
+const adapter=createEntityAdapter<campground>();
+export const initialState:CampgroundState=adapter.getInitialState({
+    selectCampground:0
+});
 export const campgroundReducer=createReducer(initialState,on(Actions.selectCampgrounds,(state,{campground})=>{
     return {
         ...state,
         selectCampground:campground
     }
 }),on(Actions.loadCampgroundsSuccess,(state,{campgrounds})=>{
-    return{
-        ...state,
-        campgrounds:campgrounds
-    }
+    return adapter.setAll(campgrounds,state);
 }))
--- frontEndCampground/src/app/store/campground/campground.selection.ts
+++ frontEndCampground/src/app/store/campground/campground.selection.ts
@@ -1,7 +1,8 @@
 import { createSelector } from "@ngrx/store";
 import { AppState } from "../../app.state";
+import { campground } from "../../../models/campground";
 
 export const selectCampgroundFeature=createSelector((state:AppState)=>state.campgrounds,(campgrounds)=>campgrounds);
-export const selectCampgroundList=createSelector(selectCampgroundFeature,(campground)=>campground.campgrounds);
+export const selectCampgroundList=createSelector(selectCampgroundFeature,(campground)=>campground.ids.map(x=><campground>campground.entities[x]));
 export const selectCampground=createSelector(selectCampgroundFeature,(campground)=>campground.selectCampground);
 export const selectCampgroundObject=createSelector(selectCampgroundList,selectCampground,(s1,s2)=>s1.find(x=>x.id==s2));
